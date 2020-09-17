5707ed4a30e51d15562f2e1e9f10fbfdf7ded4ab
Chris Parsley <ChrisP@merit.local>
2020-09-17 10:39:04 +0100

    End of section 9

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -2,12 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {map} from 'rxjs/operators';
 import {JwtHelperService} from '@auth0/angular-jwt';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  baseurl = 'http://localhost:5000/api/auth/';
+  // baseUrl = 'http://localhost:5000/api/auth/';
+  baseurl = environment.apiUrl + 'auth/';
+  
   jwtHelper = new JwtHelperService();
   decodedToken: any;
 
