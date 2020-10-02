b63a54390f56cf147a9d2da2f7ace8c9c5683f26
Chris Parsley <ChrisP@merit.local>
2020-10-02 12:08:30 +0100

    End of Section 15

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -10,5 +10,29 @@ namespace DatingApp.API.Data
         public DbSet<Value> Values { get; set;}
         public DbSet<User> Users {get; set;}
         public DbSet<Photo> Photos { get; set; }
+        public DbSet<Like> Likes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Like>()
+                .HasKey(k => new { k.LikerId, k.LikeeId });
+
+            /*
+            Define custom relationship and behaviour.
+            A user can "like" many users and a user can be "liked" by many users
+            */
+            builder.Entity<Like>()
+                .HasOne(u => u.Likee)
+                .WithMany(u => u.Likers)
+                .HasForeignKey(u => u.LikeeId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Like>()
+                .HasOne(u => u.Liker)
+                .WithMany(u => u.Likees)
+                .HasForeignKey(u => u.LikerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+        }
     }
 }
