945f5c3d5c45a8cbe737201dcf2c07549c872ad9
Chris Parsley <ChrisP@merit.local>
2020-10-06 13:03:22 +0100

    End of Section 16

--- DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
+++ DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NgxGalleryAnimation, NgxGalleryImage, NgxGalleryOptions } from '@kolkov/ngx-gallery';
+import { TabsetComponent } from 'ngx-bootstrap/tabs';
 import { User } from 'src/app/_models/user';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { UserService } from 'src/app/_services/user.service';
+
 
 @Component({
   selector: 'app-member-detail',
@@ -11,6 +13,7 @@ import { UserService } from 'src/app/_services/user.service';
   styleUrls: ['./member-detail.component.scss']
 })
 export class MemberDetailComponent implements OnInit {
+  @ViewChild('memberTabs', {static: true}) memberTabs: TabsetComponent;
   user: User;
   galleryOptions: NgxGalleryOptions[];
   galleryImages: NgxGalleryImage[];
@@ -22,6 +25,13 @@ export class MemberDetailComponent implements OnInit {
       this.route.data.subscribe(data => {
         this.user = data['user'];
       });
+      
+      this.route.queryParams.subscribe(params => {
+        const selectedTab = params['tab'];
+        console.log('Tab: ' + selectedTab);
+        this.memberTabs.tabs[selectedTab > 0 ? selectedTab : 0].active = true;
+      });
+      
 
       this.galleryOptions = [
         {
@@ -50,5 +60,9 @@ export class MemberDetailComponent implements OnInit {
     }
     return imageUrls;
 
+  }
+
+  selectTab(tabId: number) {
+    this.memberTabs.tabs[tabId].active = true;
   }
 }
